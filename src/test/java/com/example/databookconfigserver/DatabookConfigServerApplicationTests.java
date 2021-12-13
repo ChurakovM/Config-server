4b6bef565cc37531cd95dff3e7a9e06b4d7135b4
mikhailc@backbase.com <mikhailc@backbase.com>
2021-12-13 21:19:23 +0100

    first commit to the repo

---
+++ src/test/java/com/example/databookconfigserver/DatabookConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.databookconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabookConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
