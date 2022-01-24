6495b0764e61d52764801f841a404908a41b6045
mikhailc@backbase.com <mikhailc@backbase.com>
2022-01-24 20:48:11 +0100

    renamed directories

--- src/test/java/com/example/configserver/ConfigServerApplicationTests.java
+++ src/test/java/com/example/configserver/ConfigServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.databookconfigserver;
+package com.example.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DatabookConfigServerApplicationTests {
+class ConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
